be0bcd96b91ecf9de5d7b48169979e5fc2db3705
Hung Q. Nguyen <hnguyenworkstation@gmail.com>
2016-09-17 11:40:36 -0400

    finished app structure

--- SonicNews/app/src/main/java/com/greenfam/sonicnews/MainActivity.java
+++ SonicNews/app/src/main/java/com/greenfam/sonicnews/MainActivity.java
@@ -3,12 +3,17 @@ package com.greenfam.sonicnews;
 import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.IdRes;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.greenfam.sonicnews.Fragments.HomeFragment;
 import com.greenfam.sonicnews.Fragments.LocalFragment;
@@ -27,9 +32,17 @@ public class MainActivity extends AppCompatActivity
                 MessageFragment.OnFragmentInteractionListener,
                 MoreFragment.OnFragmentInteractionListener
 {
-
-    private BottomBar bottomBar;
     private ViewPager viewPager;
+    private TabLayout mTabLayout;
+    private ViewPagerAdapter adapter;
+
+
+    private int[] mTabsIcons = {
+            R.drawable.ic_favorite_white_24dp,
+            R.drawable.ic_location_on_white_24dp,
+            R.drawable.ic_update_white_24dp,
+            R.drawable.ic_local_dining_white_24dp
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,21 +50,26 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
 
-        bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-            @Override
-            public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_favorites) {
+        if (viewPager != null)
+            setupViewPager(viewPager);
 
-                }
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        if (mTabLayout != null) {
+            mTabLayout.setupWithViewPager(viewPager);
+
+            for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+                TabLayout.Tab tab = mTabLayout.getTabAt(i);
+                if (tab != null)
+                    tab.setCustomView(adapter.getTabView(i));
             }
-        });
+
+            mTabLayout.getTabAt(0).getCustomView().setSelected(true);
+        }
     }
 
     private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(this.getBaseContext(), getSupportFragmentManager());
+        adapter = new ViewPagerAdapter(this.getBaseContext(), getSupportFragmentManager());
         adapter.addFragment(new HomeFragment(), "Home");
         adapter.addFragment(new LocalFragment(), "Local");
         adapter.addFragment(new MessageFragment(), "Message");
@@ -74,6 +92,16 @@ public class MainActivity extends AppCompatActivity
         public ViewPagerAdapter(Context mContext, FragmentManager manager) {
             super(manager);
             this.mContext = mContext;
+        }
+
+        public View getTabView(int position) {
+            // Given you have a custom layout in `res/layout/custom_tab.xml` with a TextView and ImageView
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_tab, null);
+            TextView title = (TextView) view.findViewById(R.id.title);
+            title.setText(mFragmentTitle.get(position));
+            ImageView icon = (ImageView) view.findViewById(R.id.icon);
+            icon.setImageResource(mTabsIcons[position]);
+            return view;
         }
 
         @Override
